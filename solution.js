9d4ebbe65bf2ace0df4051db7b0899bd2e676b29
Asiya Farhath <asiyacode@gmail.com>
2026-06-13 22:06:45 +0530

    js-1-testing-avg

--- solution.js
+++ solution.js
@@ -1,7 +1,46 @@
-function fibonacci(n) {
-  if (n <= 1) return n;
+// SECTION 1
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
+let fullName = "Shreya";
+let age = 19;
+let semester = 3;
+let hasLaptop = true;
+let attendance = 87.5;
 
-// module.exports = { fibonacci };
+console.log(fullName);
+console.log(age);
+
+let city = "Delhi";
+let graduationYear = "2027";
+let isHosteller = true;
+let cgpa = 8.5;
+
+console.log(city);
+
+let firstName = "Arjun";
+let lastName = "Reddy";
+
+[firstName, lastName] = [lastName, firstName];
+
+console.log(firstName, lastName);
+
+let name123 = "Ravi";
+let $amount = 2000;
+let varName = "keyword";
+let isEligible = true;
+
+// Missing homeTown
+
+console.log("2053");
+console.log("462");
+console.log("162");
+console.log("76");
+
+console.log("10" + 5);
+console.log("10" * 5);
+
+let a = "5";
+let b = 2;
+let c = true;
+
+console.log(a + b + c);
+console.log(Number(a) + b + c);
